dbac247ebdedd92a73a8df0c472ad34b16c2ddf4
Frank Fenton <echelon.iiii@gmail.com>
2021-08-06 21:02:02 +1000

    🔊 Add guild id to log

--- inviteCalculations.ts
+++ inviteCalculations.ts
@@ -26,12 +26,13 @@ function determineWhichInviteWasUsed(oldInvitesForGuild: Collection<string, Invi
   return null
 }
 export async function determineIfTemporaryInviteUsedAndUpdateInviteCache(guild: Guild, cache: InviteCache) {
+  const guildLogger = logger.child({ guild: guild.id })
   return await cache.gate.workFast(guild, async () => {
     const oldInvites = cache.getGuild(guild)
-    logger.debug('fetching invites for guild %s', guild.name)
+    guildLogger.debug('fetching invites for guild %s', guild.name)
     const newInvites = await cache.fetchAndCache(guild)
     const inviteUsed = determineWhichInviteWasUsed(oldInvites, newInvites)
-    logger.debug({ inviteUsed }, 'new user used invite %s', inviteUsed?.code || 'unknown')
+    guildLogger.debug({ inviteUsed }, 'new user used invite %s', inviteUsed?.code || 'unknown')
     return inviteUsed && inviteUsed.temporary
   })
 }
--- main.ts
+++ main.ts
@@ -43,7 +43,7 @@ client.on('ready', async () => {
   logger.debug({ guilds: client.guilds.cache.map((guild) => guild.name) }, 'shard is dealing with guilds')
   for (const guild of client.guilds.cache.values()) {
     const guildInvites = await inviteCache.fetchAndCache(guild)
-    logger.debug('found %d invites for guild %s', guildInvites.size, guild.name)
+    logger.child({ guild: guild.id }).debug('found %d invites for guild %s', guildInvites.size, guild.name)
   }
   logger.info('Ready')
 })
@@ -53,41 +53,43 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
     return
   }
   const member = oldState.member || newState.member
-  logger.info(`voiceStateUpdate event with user ${member?.displayName} going from ${oldState.channel?.name} to ${newState.channel?.name}`)
+  const guildLogger = logger.child({ guild: (oldState || newState)?.guild?.id })
+  guildLogger.info(`voiceStateUpdate event with user ${member?.displayName} going from ${oldState.channel?.name} to ${newState.channel?.name}`)
   if (member && config.get('dontAddRolesToTemporaryMembers') && temporaryMembers.has(member)) {
-    logger.info({ member }, 'this user is only temporary, so we cant add roles to them')
+    guildLogger.info({ member }, 'this user is only temporary, so we cant add roles to them')
     return
   }
   try {
     const roles = calculateRoleDifference(oldState.channel, newState.channel)
     if (roles[0]?.size && oldState.member) { // something to be deleted
-      logger.debug(`${oldState.member.displayName} removed from roles ${roles[0].map(role => role.name).join(', ')}`)
+      guildLogger.debug(`${oldState.member.displayName} removed from roles ${roles[0].map(role => role.name).join(', ')}`)
       await oldState.member.roles.remove(roles[0], `They left the ${oldState.channel!.name} voice channel`)
     }
     if (roles[1]?.size && newState.member) { // something to be added
-      logger.debug(`${newState.member.displayName} added to roles ${roles[1].map(role => role.name).join(', ')}`)
+      guildLogger.debug(`${newState.member.displayName} added to roles ${roles[1].map(role => role.name).join(', ')}`)
       await newState.member.roles.add(roles[1], `They joined the ${newState.channel!.name} voice channel`)
     }
   } catch (err) {
-    logger.error(err, 'failed to mess with roles')
+    guildLogger.error(err, 'failed to mess with roles')
   }
 
 })
 
 client.on('guildMemberAdd', async (member) => {
-  logger.info({ member }, 'guildMemberAdd event fired')
+  const guildLogger = logger.child({ guild: member?.guild?.id })
+  guildLogger.info({ member }, 'guildMemberAdd event fired')
   const roleToAdd = config.get('autoAssignedRole')
   let shouldAddRole = !!roleToAdd
   if (shouldAddRole && config.get('dontAddRolesToTemporaryMembers'))  {
     const temporary = await determineIfTemporaryInviteUsedAndUpdateInviteCache(member.guild, inviteCache)
     if (temporary) {
-      logger.info('new user is only temporary')
+      guildLogger.info('new user is only temporary')
       shouldAddRole = false
       temporaryMembers.addToCache(member)
     }
   }
   if (shouldAddRole) {
-    logger.debug('adding role "%s" to user %s', roleToAdd, member.displayName)
+    guildLogger.debug('adding role "%s" to user %s', roleToAdd, member.displayName)
     try {
       const roleActual = member.guild.roles.cache.find((role) => role.name == roleToAdd)
       if (!roleActual) {
@@ -95,23 +97,26 @@ client.on('guildMemberAdd', async (member) => {
       }
       await member.roles.add(roleActual, 'They joined and everyone gets this')
     } catch (err) {
-      logger.error(err, 'could not add "%s" role to member', roleToAdd)
+      guildLogger.error(err, 'could not add "%s" role to member', roleToAdd)
     }
   }
 })
 client.on('guildMemberRemove', (member) => {
-  logger.info({ member }, 'guildMemberRemove event fired')
+  const guildLogger = logger.child({ guild: member?.guild?.id })
+  guildLogger.info({ member }, 'guildMemberRemove event fired')
   temporaryMembers.deleteFromCache(member)
 })
 client.on('inviteCreate', async (invite) => {
-  logger.debug({ invite }, 'new invite')
+  const guildLogger = logger.child({ guild: invite?.guild?.id })
+  guildLogger.debug({ invite }, 'new invite')
   await inviteCache.gate.workFast(invite.guild, async () => {
     inviteCache.addToCache(invite)
   })
 })
 client.on('inviteDelete', async (invite) => {
+  const guildLogger = logger.child({ guild: invite?.guild?.id })
   await wait(config.get('inviteDeleteWaitTimeMs')) // we wait a little before deleting them, so if they were deleted by a user add event, we have time to see that
-  logger.debug({ invite }, 'deleted invite')
+  guildLogger.debug({ invite }, 'deleted invite')
   await inviteCache.gate.workSlow(invite.guild, async () => {
     inviteCache.deleteFromCache(invite)
   })
