50b66efd188e953bd451e9d747a2d06e2b337e1d
Frank Fenton <echelon.iiii@gmail.com>
2021-08-07 00:50:39 +1000

    💄🔧 Better message formating config for pino

---
+++ pino-config/pino-pretty.config.js
@@ -0,0 +1,13 @@
+const chalk = require('chalk')
+module.exports = {
+  messageFormat: function (log, messageKey, levelLabel) {
+    const logParts = []
+    logParts.push(chalk.magenta`[${log['module'] || 'global'}]`)
+    if (log['guild']) {
+      logParts.push(chalk.yellow(`{Guild: ${log['guild']}}`))
+    }
+    logParts.push(log[messageKey])
+    return logParts.join(' - ')
+  },
+  ignore: 'pid,hostname,module,guild'
+}
